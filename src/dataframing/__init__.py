6f9ef83dcc33d6d76cf3b2e247a37ee6d98e1311
Hernan Grecco <hgrecco@gmail.com>
2023-11-29 22:01:00 -0300

    Export symbols in __init__

--- src/dataframing/__init__.py
+++ src/dataframing/__init__.py
@@ -7,3 +7,8 @@
     :copyright: (c) 2023 by Hernan E. Grecco.
     :license: BSD, see LICENSE for more details.
 """
+
+from .io import load, load_many, save, save_many
+from .transform import morph, wrap
+
+__all__ = ["save", "save_many", "load", "load_many", "morph", "wrap"]
